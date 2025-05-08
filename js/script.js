7e63c81422a49644bd06fb61bbc3d3f62e1d52a8
bharath532 <bharath978930@gmail.com>
2025-05-08 11:10:11 +0530

    8.5.25

---
+++ js/script.js
@@ -0,0 +1,268 @@
+// confirm('r u exit..!')
+
+// console.log("Hello Welcome..");
+
+// document.write('<h1>Hello surya</h1>')
+
+
+// var Name = "Surya";
+
+// var Name = "Bharath"
+
+// let Name = "Surya";
+
+// Name ="Bharath" 
+
+// console.log(Name);
+
+
+// const Num = 123456;
+
+// Num=11223
+
+// console.log(Num);
+
+//  alert('warning')
+// prompt('Enter Your Name')
+// confirm(' r u exit!')
+
+//  open('https://idmtechpark-mern.netlify.app/')
+//  close()
+
+// print()
+
+// let Num=10;
+
+// console.log(num);
+// console.log(Num.toString());
+
+// let n = "20";
+
+// console.log(n);
+// console.log(parseInt(n));
+
+
+// let Name = " Suryraj "
+// console.log(Name);
+// console.log(Name.length);
+// console.log(Name.concat("Bharath"));
+// console.log(Name+" Bhrath");
+// console.log(Name.concat(Name));
+// console.log(Name.repeat(3));
+// console.log(Name.toUpperCase());
+// console.log(Name.trim());
+
+
+// console.log(Name[1]);
+// console.log(Name.indexOf("a"));
+// console.log(Name.lastIndexOf('r'));
+
+// Name[9]="bharath"
+// console.log(Name);
+
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.abs(90));
+// console.log(Math.ceil(9.5));
+// console.log(Math.floor(999.99999));
+// console.log(Math.max(99,77));
+// console.log(Math.min(68,98));
+// console.log(Math.pow(2,5));
+// console.log(Math.random());
+// console.log(Math.round(4.5));
+// console.log(Math.sign(-9));
+
+
+
+// +,-,*,/,%
+
+// == !=
+
+// < > <= >= != == ===
+
+// AND &&  OR||  NOT !
+
+// ++  --
+
+// 0 1 AND OR NOT 
+
+// value ? true : false 
+
+// if(20<4){
+//     console.log("1st value crt..");
+    
+// }
+
+// else if((10<50)&&(60<7)){
+//     console.log("2nd Crt..");
+    
+// }
+
+// else if((4<5)||(9<7)){
+// console.log("3rd value is crt,..");
+
+// }
+
+// else{
+    // console.error("Faill");
+    
+// }
+
+
+// if(90<99){
+//     if(80<81){
+//         if(70<99){
+//             console.log(":Success");
+            
+//         }
+//     }
+// }
+
+
+
+
+let a =;
+
+switch(a){
+    case 0:
+        console.log("Zero..");
+        break;
+
+    case 1:
+        console.log("One");
+        break;
+
+    case 2:
+        console.log("Two..");
+        break;
+
+    default :
+    console.log("Error...");
+    break;
+    
+}
+
+
+// let arr = [1,2,3,4,'s',"Bharath"]
+
+// console.log(arr.concat(arr));
+
+// console.log(arr.length);
+
+// console.log(arr[0]);
+
+// arr.push(12);
+// arr.push(13)
+
+// arr.pop()
+// arr.pop()
+
+// arr.unshift('karthi')
+// arr.unshift('ee')
+
+// arr.shift()
+
+// arr.splice(0,1,'KIA','BMW');
+
+
+
+
+
+// console.log(arr);
+
+
+// const Arr = [
+//     1,2,3
+// ]
+
+// Arr=90
+
+// Arr.push("surya")
+
+// console.log(Arr);
+
+
+// const Ar = [
+//     [1,2,3,4],[5,6,7,[8,9,11,[12,13,14,[19.18,17]]]]
+// ]
+
+// let obj ={
+//     Name :"Suryaraj",
+//     Age:21,
+//     City :"ERODE",
+//     brother:
+//     {
+//         Name:"Karthick"
+//     }
+// }
+
+// console.log(obj.brother.Name);
+
+
+// let ArrObj = [
+//     {Name:"Suryraj1"},
+//     {Name:"Suryraj2"},
+//     {Name:"Suryraj3"},
+//     {Name:"Suryraj4"},
+// ]
+// console.log(ArrObj[2].Name);
+
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// let a =10
+// do{
+//     a++
+//     console.log(a);    
+// }while(a<20)
+
+    // while(a<30){
+    //     a++
+    //     console.log(a);
+    //     if(a==25){
+    //         break
+    //     }
+        
+    // }
+
+
+// for(let i=0;i<ArrObj.length;i++){
+//     console.log(ArrObj[i]);
+    
+// }
+
+
+// for(let b in ArrObj){
+//     console.log(b);    
+// }
+
+// ArrObj.forEach((i)=>{
+//     console.log(i);
+    
+// })
+
+// ArrObj.map((u)=>{
+//     console.log(u);
+    
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
